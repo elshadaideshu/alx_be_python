317d5c4024fe82934f93d227532ea2859604f21b
elshadaideshu <elshadaid48@gmail.com>
2024-09-14 03:23:03 -0700

    first add

---
+++ Desktop/python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number1 * number2
+print("addition of", number1, "and", number2, "is" ,x)
+print("subtraction of", number1, "and", number2, "is", y)
+print("multiplication of", number1, "and", number2, "is", z)
